04a504b4b21d27c58416981b7da0ceb917beadb6
potatosil <63179591+Potatosil@users.noreply.github.com>
2021-08-31 11:38:55 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,31 @@
+import pyautogui, time
+from time import sleep
+
+time = 0
+while time != 10:
+	time += 1
+	sleep(1) 
+	print("spammer waitinig.." + str(time))
+
+def spam (msg, maxMsg): 
+	count = 0
+	while count != maxMsg:
+		count += 1 
+		print("sendmessage:" + str(count))
+		pyautogui.write(msg)
+		pyautogui.press("enter")
+		if count == 5 or count == 5*2 or count == 5*3 or count == 5*4 or count == 5*5 or count == 5*6 or count == 5*7 or count == 5*8 or count == 5*9 or count == 5*20:	
+			sleep(5)
+				
+
+spam("i am a spambot made by potato", 100)
+
+sleep(2)
+
+pyautogui.write("done")
+pyautogui.press("enter")
+
+
+
+
+		
