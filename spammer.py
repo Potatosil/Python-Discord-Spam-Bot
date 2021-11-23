7d3f12ae4e9c9933754be484ec2755c22ca079d1
potatosil <63179591+Potatosil@users.noreply.github.com>
2021-11-23 10:25:41 +0530

    Made it Better
    Removed Potatoapi
    Added Input for SpamNumber and SpamText

--- spammer.py
+++ spammer.py
@@ -1,31 +1,29 @@
 import pyautogui, time
 from time import sleep
 
+spamnum = int(input(f"Input Number: "))
+spamtext = input(f"What is the Message u want to send?: ")
+
 time = 0
 while time != 10:
 	time += 1
-	sleep(1) 
+	sleep(1)
 	print("spammer waitinig.." + str(time))
 
-def spam (msg, maxMsg): 
+def spam (msg, maxMsg):
 	count = 0
 	while count != maxMsg:
-		count += 1 
+		count += 1
 		print("sendmessage:" + str(count))
 		pyautogui.write(msg)
 		pyautogui.press("enter")
-		if count == 5 or count == 5*2 or count == 5*3 or count == 5*4 or count == 5*5 or count == 5*6 or count == 5*7 or count == 5*8 or count == 5*9 or count == 5*20:	
+		if count == spamnum or count == spamnum*2 or count == spamnum*3 or count == spamnum*4 or count == spamnum*spamnum or count == spamnum*6 or count == spamnum*7 or count == spamnum*8 or count == spamnum*9 or count == spamnum*20:
 			sleep(5)
-				
 
-spam("i am a spambot made by potato, sub to potato", 100)
+
+spam(spamtext, spamnum)
 
 sleep(2)
 
 pyautogui.write("done")
 pyautogui.press("enter")
-
-
-
-
-		
